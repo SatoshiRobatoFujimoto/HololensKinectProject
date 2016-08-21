b08918c058ff6df556760f2899d80f5833c8960c
alexasiu <alexasiu4@gmail.com>
2016-08-21 03:35:45 -0700

    working point cloud

---
+++ PointCloud/DataManager.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class DataManager : MonoBehaviour {
+
+    public string depthFile;
+    public string colorFile;
+    public string frameFile;
+    //public string tableXFile;
+    //public string tableYFile;
+
+    //private Vector3[] _CameraData;
+    private ushort[] _DepthData;
+    private float[] _ColorData;
+    private Vector2[] _TableData;
+
+    public int frameWidth { get; private set; }
+    public int frameHeight { get; private set; }
+    public bool initialized { get; private set; }
+
+    /*
+    public Vector3[] GetCameraData()
+    {
+        return _CameraData;
+    }
+    */
+
+    public ushort[] GetDepthData()
+    {
+        return _DepthData;
+    }
+
+    public float[] GetColorData()
+    {
+        return _ColorData;
+    }
+
+    /*
+    public Vector2[] GetTableData()
+    {
+        return _TableData;
+    }
+    */
+
+    // Use this for initialization
+    void Start () {
+
+        initialized = false;
+
+        /*frameWidth = 130; // tableXReader.ReadInt32();
+        frameHeight = 424; // tableXReader.ReadInt32();*/
+
+        using (BinaryReader frameReader = new BinaryReader(File.Open(frameFile, FileMode.Open)))
+        {
+            frameWidth = frameReader.ReadInt32();
+            frameHeight = frameReader.ReadInt32();
+        }
+
+        initialized = true;
+
+        /*
+        // read table x values
+        using (BinaryReader tableXReader = new BinaryReader(File.Open(tableXFile, FileMode.Open)))
+        {
+            frameWidth = 130; // tableXReader.ReadInt32();
+            frameHeight = 424; // tableXReader.ReadInt32();
+            int sizeArray = tableXReader.ReadInt32();
+            Debug.Log("Frame height: " + frameHeight);
+            Debug.Log(sizeArray);
+            _TableData = new Vector2[sizeArray];
+            for (int i = 0; i < _TableData.Length; i++)
+            {
+                _TableData[i] = new Vector2(tableXReader.ReadInt32(), 0);
+            }
+            //tableXReader.Close();
+        }
+
+        // read table y values
+        using (BinaryReader tableYReader = new BinaryReader(File.Open(tableYFile, FileMode.Open)))
+        {
+            for (int i = 0; i < _TableData.Length; i++)
+            {
+                _TableData[i].y = (float)tableYReader.ReadInt32();
+            }
+            //tableYReader.Close();
+        }
+        */
+
+        Debug.Log("Frame width: " + frameWidth);
+        // read depth text file
+        using (BinaryReader depthReader = new BinaryReader(File.Open(depthFile, FileMode.Open)))
+        {
+
+            int sizeArray = depthReader.ReadInt32();
+            Debug.Log(sizeArray);
+            _DepthData = new ushort[sizeArray];
+
+            for (int i = 0; i < _DepthData.Length; i++)
+            {
+                _DepthData[i] = (ushort) depthReader.ReadUInt16();
+            }
+            depthReader.Close();
+        }
+
+        // read color values
+        using (BinaryReader colorReader = new BinaryReader(File.Open(colorFile, FileMode.Open)))
+        {
+            int sizeArray = colorReader.ReadInt32();
+            _ColorData = new float[sizeArray];
+            Debug.Log(sizeArray);
+            for (int i = 0; i < _ColorData.Length; i++)
+            {
+                _ColorData[i] = (float)colorReader.ReadByte() / 255f;
+                //float test = (float)colorReader.ReadByte();
+            }
+            //colorReader.Close();
+        }
+        
+        /*
+        _CameraData = new Vector3[frameHeight * frameWidth];
+        // Reconstruct XYZ camera space table
+        for (int x = 0; x < frameWidth; x++)
+        {
+            for (int y = 0; y < frameHeight; y++)
+            {
+                int index = (y * frameWidth) + x;
+                Vector2 lutValue = _TableData[index];
+                    ushort depth = _DepthData[index];
+                    _CameraData[index] = new Vector3(
+                        lutValue.x * depth * 0.001f,
+                        lutValue.y * depth * 0.001f,
+                        depth * 0.001f);
+
+            }
+       }
+        */
+
+    } // end start
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+}
---
+++ PointCloud/PointCloudRender.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PointCloudRender : MonoBehaviour
+{
+
+    public GameObject DataManager;
+
+    // Mesh parameters
+    private const int _DownsampleSize = 4; // to render less than 65k points
+    private Mesh _Mesh;
+    private Vector3[] _Vertices;
+    private Color[] _Colors;
+    private int[] _Index;
+
+    // Kinect parsed data
+    private DataManager _DataManager;
+    private Vector3[] _CameraData;
+    private float[] _ColorData;
+    private int width;  // frame width
+    private int height; // frame height
+
+    private const int BYTES_PER_PIXEL = 4;
+
+    bool refresh = true;
+
+    void Start()    
+    {
+        _DataManager = GetComponent<DataManager>();
+        if (_DataManager != null)
+        {
+            if (_DataManager.initialized)
+            {
+                width = _DataManager.frameWidth;
+                height = _DataManager.frameHeight;
+
+                // create a fixed size mesh
+                // mesh vertex position/color will be updated every frame
+                _Mesh = new Mesh();
+                GetComponent<MeshFilter>().mesh = _Mesh;
+                CreateMesh(width, height);
+            }
+        }
+    }
+
+    void CreateMesh(int width, int height)
+    {
+        _Vertices = new Vector3[width * height];
+        _Index = new int[width * height];
+        _Colors = new Color[width * height];
+
+        for (int x = 0; x < width; x++ )
+        {
+            for (int y = 0; y < height; y++ )
+            {
+                int i = (y * width) + x;
+                _Index[i] = i;
+
+                _Vertices[i] = new Vector3(x, -y, 0);
+
+                _Colors[i] = new Color(0,0,0,1);
+            }
+        }
+
+        /*for (int i = 0; i < points.Length; ++i)
+        {
+            points[i] = new Vector3(UnityEngine.Random.Range(-10, 10), 
+            UnityEngine.Random.Range(-10, 10), 
+            UnityEngine.Random.Range(-10, 10));
+            index[i] = i;
+            colors[i] = new Color(UnityEngine.Random.Range(0.0f, 1.0f), 
+            UnityEngine.Random.Range(0.0f, 1.0f), 
+            UnityEngine.Random.Range(0.0f, 1.0f), 1.0f);
+        }*/
+
+        _Mesh.vertices = _Vertices;
+        _Mesh.colors = _Colors;
+        _Mesh.SetIndices(_Index, MeshTopology.Points, 0);
+
+    }
+
+    void Update()
+    {
+        
+        _DataManager = DataManager.GetComponent<DataManager>();
+        if (_DataManager == null)
+        {
+            return;
+        }
+        
+        if (!_DataManager.initialized)
+        {
+            return;
+        }
+
+        if (_Mesh == null)
+        {
+            width = _DataManager.frameWidth;
+            height = _DataManager.frameHeight;
+            _Mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = _Mesh;
+            CreateMesh(width, height);
+        }
+
+        _CameraData = _DataManager.GetCameraData();
+        if (_CameraData == null)
+        {
+            return;
+        }
+
+        _ColorData = _DataManager.GetColorData();
+        if (_ColorData == null )
+        {
+            return;
+        }
+
+        RefreshMesh(width, height);
+
+    }
+
+    void RefreshMesh(int width, int height)
+    {
+
+        int colorCount = 0;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int i = (y * width) + x;
+                _Index[i] = i;
+                _Vertices[i].z = _CameraData[i].z;
+
+                /*
+                _Colors[i] = new Color(UnityEngine.Random.Range(0.0f, 1.0f),
+                                       UnityEngine.Random.Range(0.0f, 1.0f),
+                                       UnityEngine.Random.Range(0.0f, 1.0f), 1.0f);
+                                       */
+               
+                if (colorCount <= ( (width*height) - BYTES_PER_PIXEL) )
+                {
+
+                    _Colors[i] = new Color( _ColorData[i + 0],
+                                            _ColorData[i + 1],
+                                            _ColorData[i + 2],
+                                            1.0f);
+
+                    colorCount += BYTES_PER_PIXEL;
+                }
+                
+
+            }
+        }
+
+        _Mesh.vertices = _Vertices;
+        _Mesh.colors = _Colors;
+        _Mesh.SetIndices(_Index, MeshTopology.Points, 0);
+
+    }
+
+}
